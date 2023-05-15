4baf90f513d9e1747dd123f1bac6f477d6f8693e
chintal30 <chintalpatel30@gmail.com>
2023-05-15 00:46:57 -0400

    Added word, excel and readme file

---
+++ readme.md
@@ -0,0 +1,13 @@
+# ToDoListQA
+QA Challenge
+1. For Selenium testing using Java:
+  1. Please copy the index.html path in config.properties file at amyint.url
+  2. For reporting Path is : reports/SparkActimePlugin.html
+  3. To run the tests Path is : src/test/java/ambyint/ToDoList/TestRunners/BaseTest.java
+
+2. Excel file named : TestPlan.xlsx 
+   for a brief Test Plan on Sheet1 named: Project Details, 
+   for Test Scenarios and Test Cases: Sheet2 named: TestData
+3. Word file named : QA_Test_Plan
+   for Test Plan
+   
--- src/test/java/ambyint/ToDoList/TestRunners/BaseTest.java
+++ src/test/java/ambyint/ToDoList/TestRunners/BaseTest.java
@@ -1,11 +1,13 @@
 package ambyint.ToDoList.TestRunners;
 
+import ambyint.ToDoList.utilities.BrowserFactory;
+import io.cucumber.java.AfterAll;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"src/test/java/ambyint/ToDoList/features"},
         glue = {"ambyint.ToDoList.stepDefinitions"},
-        tags = "@low",
+        tags = "",
         plugin={"html:reports/SparkActimePlugin.html",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
